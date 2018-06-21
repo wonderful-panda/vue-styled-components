a576858acfa593359e240ea4a608e7e8e5629ce7
Iwata Hidetaka <iwata0303@gmail.com>
2018-06-21 22:29:44 +0900

    fix: named slots was not passed down correctly

--- src/models/StyledComponent.js
+++ src/models/StyledComponent.js
@@ -1,10 +1,5 @@
 export default (ComponentStyle) => {
   const createStyledComponent = (target, rules, props) => {
-    const prevProps = target && typeof target !== 'string'
-      ? (typeof target === 'object' ? target.props : (typeof target === 'function' ? target.options.props : {}))
-      : {}
-    const mergedProps = Object.assign({}, prevProps, props)
-
     const componentStyle = new ComponentStyle(rules)
     let getGeneratedClassName
     if (rules.filter(r => r instanceof Function).length === 0) {
@@ -16,18 +11,26 @@ export default (ComponentStyle) => {
 
     const StyledComponent = {
       functional: true,
-      props: mergedProps,
-      render: function (createElement, { props, data, children }) {
+      render: function (createElement, { props, data, slots }) {
         const generatedClassName = getGeneratedClassName(props)
-        let staticClass
         if (data.staticClass) {
-          staticClass = data.staticClass + ' ' + generatedClassName
+          data.staticClass = data.staticClass + ' ' + generatedClassName
         } else {
-          staticClass = generatedClassName
+          data.staticClass = generatedClassName
+        }
+        const children = []
+        const slots_ = slots()
+        for (const slotName in slots_) {
+          const slot = slots_[slotName]
+          if (slotName === 'default') {
+            children.push(slot)
+          } else {
+            children.push(createElement('template', { slot: slotName }, slot))
+          }
         }
         return createElement(
           target,
-          Object.assign({}, data, { staticClass }),
+          data,
           children
         )
       }
